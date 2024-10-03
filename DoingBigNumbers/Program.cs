36a6fbfb3307ba61b4ecc137e0d879617e74b8f8
Al Stone <astone@heckgrammar.co.uk>
2024-10-03 09:41:01 +0100

    Add project files.

---
+++ DoingBigNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+namespace DoingBigNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger n = 1;
+            for(int i = 1; i <= 100; i++)
+            {
+                n = BigInteger.Multiply(n, i);
+            }
+            Console.WriteLine(n.ToString());
+        }
+    }
+}
